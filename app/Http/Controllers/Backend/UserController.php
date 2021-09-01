048a8120904e3595453dc7c4dd54a7975f47ef4c
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2021-09-01 10:15:46 +0700

    #9 Showing Data On View User Page
    1. Copy data yg ada pada table users.
    2. Update file migration (menambahkan field 'usertype') lalu save.
    3.  Pada table migration on database,  update data pada kolom batch menjadi '2' yg ada pada Id = 1.
    4. lalu rollback dan migration lagi.
    5. insert kan data yg sudah disimpan secara manual di database.

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -8,10 +8,10 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function UserView(){
+    public function UserView()
+    {
         // $alldata = User::all();
-        $data ['Alldata'] = User::all();
+        $data['alldata'] = User::all();
         return view('backend.user.view_user', $data);
     }
-
 }
