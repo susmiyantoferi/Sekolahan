8d9fc5f16cf1027a556f6ce01b404e4e7fd3a529
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2021-09-03 10:39:58 +0700

    #13 Add SweetAlert2

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -59,6 +59,45 @@
 	<script src="{{ asset('backend/js/template.js') }}"></script>
 	<script src="{{ asset('backend/js/pages/dashboard.js') }}"></script>
 
+  {{-- SweetAlert2 --}}
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+  <script type="text/javascript">
+  $(function(){
+      $(document).on('click','#delete',function(e){
+        e.preventDefault();
+        var link = $(this).attr("href");
+
+
+        swal.fire({
+  title: 'Are you sure?',
+  text: "Delete This Data?",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed ){
+    window.location.href = link
+    swal.fire(
+    'Deleted!',
+    'Data Deleted Successfully.',
+    'success'
+  )
+  }
+  
+})
+
+
+      });
+
+  });
+
+  </script>
+
+
+{{-- Toastr --}}
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <script>
--- resources/views/backend/user/view_user.blade.php
+++ resources/views/backend/user/view_user.blade.php
@@ -56,7 +56,7 @@
                               <td>{{ $user->email }}</td>
                               <td>
                                 <a href="{{ route('user.edit',$user->id) }}" class="btn btn-info">Edit</a>
-                                <a href="{{ route('user.delete',$user->id) }}" class="btn btn-danger">Delete</a>
+                                <a href="{{ route('user.delete',$user->id) }}" class="btn btn-danger" id="delete">Delete</a>
                               </td>
                               
                           </tr>
