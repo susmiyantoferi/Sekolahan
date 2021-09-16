0f4e582f66ef55411211b6fa10743f4d7129109d
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2021-09-16 10:52:13 +0700

    #31 Group By, Join Data To Show On View & Setup Edit Page On Fee Category Amount

--- app/Http/Controllers/Backend/Setup/FeeAmountController.php
+++ app/Http/Controllers/Backend/Setup/FeeAmountController.php
@@ -13,7 +13,8 @@ class FeeAmountController extends Controller
 {
     public function ViewFeeAmount()
     {
-        $data['allData'] = FeeCategoryAmount::all();
+        // $data['allData'] = FeeCategoryAmount::all();
+        $data['allData'] = FeeCategoryAmount::select('fee_category_id')->groupBy('fee_category_id')->get();
         return view('backend.setup.fee_amount.view_fee_amount', $data);
     }
 
@@ -24,17 +25,16 @@ class FeeAmountController extends Controller
         return view('backend.setup.fee_amount.add_fee_amount', $data);
     }
 
-    public function FeeAmountStore(Request $request){
+    public function FeeAmountStore(Request $request)
+    {
         $countClass = count($request->class_id);
         if ($countClass != Null) {
-            for ($i=0; $i < $countClass; $i++) { 
+            for ($i = 0; $i < $countClass; $i++) {
                 $fee_amount = new FeeCategoryAmount();
                 $fee_amount->fee_category_id = $request->fee_category_id;
                 $fee_amount->class_id = $request->class_id[$i];
                 $fee_amount->amount = $request->amount[$i];
                 $fee_amount->save();
-
-
             } // end for loop
         } // end if condition
 
@@ -44,5 +44,15 @@ class FeeAmountController extends Controller
         );
 
         return redirect()->route('fee.amount.view')->with($notification);
+    }
+
+    public function FeeAmountEdit($fee_category_id)
+    {
+        $data['editData'] = FeeCategoryAmount::where('fee_category_id', $fee_category_id)->orderBy('class_id', 'asc')->get();
+        // dd($data['editData']->toArray());
+
+        $data['fee_categories'] = FeeCategory::all();
+        $data['classes'] = StudentClass::all();
+        return view('backend.setup.fee_amount.edit_fee_amount', $data);
     }
 }
