333eb5134f03bc34ae0b64c0f025a4d89b348806
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2021-09-16 20:28:27 +0700

    #32 Update Data On Fee Category Amount Page

--- app/Http/Controllers/Backend/Setup/FeeAmountController.php
+++ app/Http/Controllers/Backend/Setup/FeeAmountController.php
@@ -55,4 +55,36 @@ class FeeAmountController extends Controller
         $data['classes'] = StudentClass::all();
         return view('backend.setup.fee_amount.edit_fee_amount', $data);
     }
+
+    public function FeeAmountUpdate(Request $request, $fee_category_id)
+    {
+        if ($request->class_id == null) {
+            $notification = array(
+                'message' => 'Sorry You Do Not Select Any Class Amount',
+                'alert-type' => 'error',
+            );
+
+            return redirect()->route('fee.amount.edit', $fee_category_id)->with($notification);
+        } else {
+
+            $countClass = count($request->class_id);
+            FeeCategoryAmount::where('fee_category_id', $fee_category_id)->delete();
+            for ($i = 0; $i < $countClass; $i++) {
+                $fee_amount = new FeeCategoryAmount();
+                $fee_amount->fee_category_id = $request->fee_category_id;
+                $fee_amount->class_id = $request->class_id[$i];
+                $fee_amount->amount = $request->amount[$i];
+                $fee_amount->save();
+            } // end for loop
+
+
+        } //end if else
+
+        $notification = array(
+            'message' => 'Data Fee Category Amount Updated Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->route('fee.amount.view')->with($notification);
+    }
 }
