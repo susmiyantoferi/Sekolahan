20887c813854518b3bcf3cfb0d525940e93157e5
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2021-10-06 11:13:33 +0700

    #50 Setup Student Registration Page

--- app/Http/Controllers/Backend/Student/StudentRegistController.php
+++ app/Http/Controllers/Backend/Student/StudentRegistController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Backend\Student;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AssignStudent;
+use App\Models\User;
+use App\Models\DiscountStudent;
 
 class StudentRegistController extends Controller
 {
-    //
+    public function ViewStudentRegist()
+    {
+        $data['allData'] = AssignStudent::all();
+        return view('backend.student.student_regist.student_view', $data);
+    }
 }
