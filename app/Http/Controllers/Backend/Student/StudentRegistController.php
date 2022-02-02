e798515f599d1b70a1ed547414954177bd4331e4
DESKTOP-TV681AN\Feri Susmiyanto <irefmcr@gmail.com>
2022-02-02 10:24:04 +0700

    #54 Insert Multiple Data To User Table

--- app/Http/Controllers/Backend/Student/StudentRegistController.php
+++ app/Http/Controllers/Backend/Student/StudentRegistController.php
@@ -11,6 +11,7 @@ use App\Models\StudentShift;
 use App\Models\StudentYear;
 use App\Models\User;
 use App\Models\DiscountStudent;
+use Illuminate\Support\Facades\DB;
 
 class StudentRegistController extends Controller
 {
@@ -31,5 +32,58 @@ class StudentRegistController extends Controller
 
     public function AddStudentStore(Request $request)
     {
-    }
+        DB::transaction(function () use ($request) {
+            $checkYear = StudentYear::find($request->year_id)->name;
+            $student = User::where('usertype', 'Student')->orderBy('id', 'DESC')->first();
+
+            if ($student == null) {
+                $firstReg = 0;
+                $studentId = $firstReg + 1;
+                if ($studentId < 10) {
+                    $id_no = '000' . $studentId;
+                } elseif ($studentId < 100) {
+                    $id_no = '00' . $studentId;
+                } elseif ($studentId < 1000) {
+                    $id_no = '0' . $studentId;
+                }
+            } else {
+                $student = User::where('usertype', 'Student')->orderBy('id', 'DESC')->first()->id;
+                $studentId = $student + 1;
+                if ($student == null) {
+                    $firstReg = 0;
+                    $studentId = $firstReg + 1;
+                    if ($studentId < 10) {
+                        $id_no = '000' . $studentId;
+                    } elseif ($studentId < 100) {
+                        $id_no = '00' . $studentId;
+                    } elseif ($studentId < 1000) {
+                        $id_no = '0' . $studentId;
+                    }
+                }
+            } //end else
+
+            $final_id_no =  $checkYear . $id_no;
+            $user = new User();
+            $code = rand(000, 999);
+            $user->id_no = $final_id_no;
+            $user->password = bcrypt($code);
+            $user->usertype = 'Student';
+            $user->name = $request->name;
+            $user->f_name = $request->f_name;
+            $user->m_name = $request->m_name;
+            $user->mobile = $request->mobile;
+            $user->address = $request->address;
+            $user->gender = $request->gender;
+            $user->religion = $request->religion;
+            $user->dob = date('Y-m-d', strtotime($request->dob));
+
+            if ($request->file('image')) {
+                $file = $request->file('image');
+                $filename = date('YmdHi') . $file->getClientOriginalName();
+                $file->move(public_path('upload/student_images'), $filename);
+                $user['image'] = $filename;
+            }
+            $user->save();
+        });
+    } //end method
 }
