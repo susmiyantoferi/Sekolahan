26ddd6fc084c171351374f82e602578a5827ce17
feri susmiyanto <irefmcr@gmail.com>
2022-07-19 10:27:02 +0700

    #92 Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('users')->insert([
+            'usertype' => 'Admin',
+            'name' => 'Feri susmiyanto',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('12345'),
+            'mobile' => '081829666',
+            'address' => 'Indonesia',
+            'gender' => 'Male',
+            'role' => 'Admin',
+        ]);
     }
 }
